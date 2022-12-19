661fc10856ab562907163272fe1651aec1f835e0
Julien Bayle <100574740+404Azuro@users.noreply.github.com>
2022-12-19 08:43:49 +0100

    Premier commit
    -

---
+++ Freeledge/index.php
@@ -0,0 +1,199 @@
+<?php include ("header.php"); ?>
+
+    <div class="video_container-home">
+        <video id="background_video-home" autoplay muted loop preload="auto">
+            <source src="images/videobureau.mp4" type="video/mp4">
+        </video>
+    </div>
+
+<!-- Haut de page -->
+
+    <section class="top_container-home center">
+        <p class="welcometext-home">Bienvenue chez Freeledge</p>
+        <h1 class="searchbar_title-home">Vous êtes à la recherche d'une formation ?</h1>
+        <div class="searchbar-home">
+            <input type="text" placeholder="Insérer des mots-clés ...">
+            <a href="#">
+                <i class="fa-solid fa-magnifying-glass searchbar-icon"></i>
+            </a>
+        </div>
+        <p class="searchbar_exemples-home">Ex. : Excel, Word, Photoshop, Java, Agile...</p>
+        <div class="review_stars-home">
+            <p>4,8/5</p>
+            <div class="stars_container-home">
+                <i class="fa-solid fa-star checked"></i>
+                <i class="fa-solid fa-star checked"></i>
+                <i class="fa-solid fa-star checked"></i>
+                <i class="fa-solid fa-star checked"></i>
+                <i class="fa-solid fa-star checked"></i>
+            </div>
+            <p class="reviews_verified-home">Avis vérifiés</p>
+        </div>
+    </section>
+
+
+<!-- Partie financement de formation -->
+
+    <section class="finance_container-home">
+        <div class="center">
+            <h2 class="subtitle-home">Financez votre formation en 2023</h2>
+            <p class="description_subtitle-home">Nos conseillers sont là pour vous aider à activer vos leviers de financement.</p>
+            <div class="finance_cards_bloc-home">
+                <a href="#">
+                    <article class="finance_card-home">
+                        <span class="finance_card_tag-home">Entreprises</span>
+                        <h3 class="finance_card_title-home">Fonds National de l'Emploi (FNE)</h3>
+                        <img src="images/photobureau1.png" alt="Image de la card Fonds National pour l'Emploi (FNE)">
+                    </article>
+                </a>
+                <a href="#">
+                    <article class="finance_card-home">
+                        <span class="finance_card_tag-home">Particuliers</span>
+                        <h3 class="finance_card_title-home">Compte personnel de formation (CPF)</h3>
+                        <img src="images/photobureau2.png" alt="Image de la card Fonds National pour l'Emploi (FNE)">
+                    </article>
+                </a>
+                <a href="#">
+                    <article class="finance_card-home">
+                        <span class="finance_card_tag-home">Entreprises</span>
+                        <h3 class="finance_card_title-home">Actions collectives & négociées par les OPCO</h3>
+                        <img src="images/photobureau3.png" alt="Image de la card Fonds National pour l'Emploi (FNE)">
+                    </article>
+                </a>
+                <a href="#">
+                    <article class="finance_card-home">
+                        <span class="finance_card_tag-home">Entreprises et particuliers</span>
+                        <h3 class="finance_card_title-home">Préparation opérationnelle à l'emploi (POE)</h3>
+                        <img src="images/photobureau4.png" alt="Image de la card Fonds National pour l'Emploi (FNE)">
+                    </article>
+                </a>
+            </div>
+        </div>
+    </section>
+
+<!-- Partie prochaines sessions de formation à venir -->
+
+    <section class="next_sessions_container-home">
+        <div class="center">
+            <h2 class="subtitle-home">Nos prochaines sessions garanties</h2>
+            <div class="next_sessions_card-home">
+                <a href="#">
+                    <article class="session_card-home">
+                        <div class="session_card_description-home">
+                            <h3 class="session_card_description_title-home">
+                                Développez la qualité dans vos relations clients
+                            </h3>
+                            <div class="session_card_review-home">
+                                <div class="session_card_stars-home">
+                                    <i class="fa-solid fa-star checked"></i>
+                                    <i class="fa-solid fa-star checked"></i>
+                                    <i class="fa-solid fa-star checked"></i>
+                                    <i class="fa-solid fa-star checked"></i>
+                                    <i class="fa-solid fa-star"></i>
+                                </div>
+                                <p class="session_card_note-home">
+                                    4/5
+                                </p>
+                            </div>
+                        </div>
+                    </article>
+                </a>
+                <a href="#">    
+                    <article class="session_card-home">
+                        <div class="session_card_description-home">
+                            <h3 class="session_card_description_title-home">
+                                Java - Formation avancée
+                            </h3>
+                            <div class="session_card_review-home">
+                                <div class="session_card_stars-home">
+                                    <i class="fa-solid fa-star checked"></i>
+                                    <i class="fa-solid fa-star checked"></i>
+                                    <i class="fa-solid fa-star checked"></i>
+                                    <i class="fa-solid fa-star checked"></i>
+                                    <i class="fa-solid fa-star checked"></i>
+                                </div>
+                                <p class="session_card_note-home">
+                                    5/5
+                                </p>
+                            </div>
+                        </div>
+                    </article>
+                </a>
+                <a href="#">    
+                    <article class="session_card-home">
+                        <div class="session_card_description-home">
+                            <h3 class="session_card_description_title-home">
+                                Développez la qualité dans vos relations clients
+                            </h3>
+                            <div class="session_card_review-home">
+                                <div class="session_card_stars-home">
+                                    <i class="fa-solid fa-star checked"></i>
+                                    <i class="fa-solid fa-star checked"></i>
+                                    <i class="fa-solid fa-star checked"></i>
+                                    <i class="fa-solid fa-star checked"></i>
+                                    <i class="fa-solid fa-star"></i>
+                                </div>
+                                <p class="session_card_note-home">
+                                    4/5
+                                </p>
+                            </div>
+                        </div>
+                    </article>
+                </a>
+            </div>
+            <a href="#">
+                <button class="btn_next_sessions-home" type="button">
+                    Consultez toutes nos sessions
+                </button>
+            </a>
+        </div>
+    </section>
+
+<!-- Partie reconversion -->
+
+    <section class="reconversion_job_container-home">
+        <div class="center">
+            <h2 class="reconversion_job_container_title-home">Vous souhaitez vous reconvertir professionnellement ?</h2>
+            <p class="reconversion_job_container_description-home">Découvrez toutes les offres de reconversion disponibles chez Freeledge</p>
+            <div class="reconversion_job_cards_container-home">
+                <a class="reconversion_job_card_link-home" href="#">
+                    <article class="reconversion_job_card-home">
+                        <p class="reconversion_job_card_tag-home">Centre Est</p>
+                        <h3 class="reconversion_job_card_title-home">Développeur(euse) Web</h3>
+                        <img src="images/photobureau3.png" alt="Image de la formation [nom de la formation]">
+                        <h4 class="reconversion_job_card_date-home">Du 12 décembre 2022 au 13 février 2023 - 38 jours (259h00)</h4>
+                    </article>
+                </a>
+                <a class="reconversion_job_card_link-home" href="#">
+                    <article class="reconversion_job_card-home">
+                        <p class="reconversion_job_card_tag-home">Nord Ouest</p>
+                        <h3 class="reconversion_job_card_title-home">Expert(e) en Webmarketing</h3>
+                        <img src="images/photobureau4.png" alt="Image de la formation [nom de la formation]">
+                        <h4 class="reconversion_job_card_date-home">Du 12 décembre 2022 au 18 mars 2023 - 54 jours (379h00)</h4>
+                    </article>
+                </a>
+            </div>
+        </div>
+    </section>
+
+<!-- Partie entreprises avec qui nous avons travaillé -->
+
+    <section class="collaborateurs_container-home">
+        <h3 class="collaborateurs_title-home">Nous avons travaillé avec</h3>
+        <div class="collaborateurs_slide-home">
+            <span class="collaborateurs_slide_image-home">
+                <img src="images/collab/logodouanes.png" alt="Logo Douanes & Droits Indirects">
+            </span>
+            <span class="collaborateurs_slide_image-home">
+                <img src="images/collab/logoina.svg" alt="Logo INA">
+            </span>
+            <span class="collaborateurs_slide_image-home">
+                <img src="images/collab/logocpme.png" alt="Logo Douanes & Droits Indirects">
+            </span>
+            <span class="collaborateurs_slide_image-home">
+                <img src="images/collab/logointerfor.png" alt="Logo Douanes & Droits Indirects">
+            </span>
+        </div>
+    </section>
+
+<?php include ("footer.php"); ?>
